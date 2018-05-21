98edd312c0071a0cd9c4c4257235a5686e016e03
Duncan Hancq <duncan.hancq@gmail.com>
2018-05-21 23:02:52 +0200

    MAJ du 20.05

--- index.php
+++ index.php
@@ -1,30 +1,49 @@
 <?php
-require_once 'init/init.inc.php'; // Fichier d'initialisation : BSD & Debug
-require_once 'class/class_template.php'; // moteur de rendu
-require_once 'class/class_template_bak.php'; // moteur de rendu
+require('init/init.inc.php'); // Fichier d'initialisation : BSD & Debug
+require('class/class_template.php'); // moteur de rendu
+require('class/class_template_bak.php'); // moteur de rendu
 
-function notFound($e){
-  echo "404 page not found boï " . $e . '\n GET : ' . debug($_GET['p']);
-
-  exit(0);
-}
-
-$isFound = false;
+$pageMatch = true;
 $getUrl = isset($_GET['p']) ? $_GET['p'] : null;
-echo $getUrl;
+
 /****************************************************************
 ---------------BACK OFFICE
 ****************************************************************/
 
-if (isset($_SESSION['user']))
+if (isset($_SESSION['user'])) // si l'utilisateur est connecté...
 {
   switch ($getUrl)
   {
-    case "add-user" :
+    case "user":
+      $userDisplay = null;
+      $getUser = $pdo->query('SELECT id_user AS id, username, role FROM user');
+      $nbrCol = $getUser->columnCount();
+      $userDisplay .= '<tr>';
+      for($i = 0; $i < $nbrCol; $i++)
+      {
 
-      $displayTemplatePage = new TemplateBak('template/add-user.html');
-      $displayTemplatePage->replaceContent('##TITLE##', 'Ajout membre');
-      $displayTemplatePage->replaceContent('##H1##', 'Ajout membre');
+          $nomCol = $getUser->getColumnMeta($i);//A chaque tour de boucle je récupère les intitulés de mes champs
+          $userDisplay .= '<th>' . ucfirst($nomCol['name']) . '</th>';
+
+      }
+      $userDisplay .= '<th>Modif.</th><th>Suppr.</th></tr>';
+      $userDisplay .= '</tr>';
+
+      while($row = $getUser->fetch())
+      {
+        $userDisplay .= '<tr><td>'.$row['id'].'</td><td>'.$row['username'].'</td><td>'.$row['role'].'</td><td>0</td><td>X</td>';
+      }
+
+      $displayTemplatePage = new TemplateBak('template/user.html');
+      $displayTemplatePage->replaceContent('##H2##', 'Gestion des utilisateurs');
+      $displayTemplatePage->replaceContent('##USER-DISPLAY##', $userDisplay);
+
+    break;
+
+    case "user-add" :
+      $displayTemplatePage = new TemplateBak('template/user-add.html');
+      $displayTemplatePage->replaceContent('##TITLE##', 'Ajouter membre');
+      $displayTemplatePage->replaceContent('##H2##', 'Ajouter membre');
 
       if(isset($_POST['ajoutmbr']))
       {
@@ -49,13 +68,15 @@ if (isset($_SESSION['user']))
           ':email'    => htmlspecialchars($email),
           ':token'    => token()
         ]);
-        $isFound = true;
+
       }
 
     break;
 
     default :
+      $pageMatch = false;
       continue;
+
     break;
   }
 
@@ -68,7 +89,7 @@ if (isset($_SESSION['user']))
 
 switch($getUrl)
 {
-  case null :
+  case null:
   case "accueil":
 
     $displayTemplatePage = new Template('template/index.html');
@@ -77,7 +98,7 @@ switch($getUrl)
 
   break;
 
-  case "bullant" :
+  case "bullant": // Page de connexion
 
     if(isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion')
     {
@@ -109,23 +130,22 @@ switch($getUrl)
 
   break;
 
-  case "logout" :
+  case "logout": // Page de déconnexion
 
     session_destroy();
-    header("Refresh:0; url=".URL."?page=accueil");
+    header("Refresh:0; url=".URL."?p=accueil");
     exit();
 
   break;
 
 
 
-  default : if($isFound === false)
-  {
-    notFound("1");
-  }
-  else{
-    continue;
-  }
+  default: // Aucune page détecté, redirection sur la page 404
+
+    if ($pageMatch == false){
+      $displayTemplatePage = new Template('template/404.html');
+    }
+    
   break;
 
 } // FIN du switch
